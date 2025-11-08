63296f7dbb9218f9ef4f260a4db169a96cb4404f
brendancapple <35118457+brendancapple@users.noreply.github.com>
2025-11-08 16:24:00 -0500

    added new main file for GUI

--- database.py
+++ database.py
@@ -1,11 +1,3 @@
-#################
-#   MediAppl    #
-# Brendan Apple #
-#################
-#
-# Media Organizer for Local Files w/o Changing File Structures
-#
-#
 import os
 import util
 
---
+++ main.py
@@ -0,0 +1,11 @@
+#################
+#   MediAppl    #
+# Brendan Apple #
+#################
+#
+# Media Organizer for Local Files w/o Changing File Structures
+#
+#
+
+from PyQt5.QtWidgets import QApplication, QWidget
+

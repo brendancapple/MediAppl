6c4ea7ea369382c7259261f1d0e2a6ef84893011
brendancapple <35118457+brendancapple@users.noreply.github.com>
2026-04-02 10:59:08 -0400

    Refactored database file detection to use from_file

--- src/database.py
+++ src/database.py
@@ -83,12 +83,7 @@ class Entry:
         entry_res = (0, 0)
         entry_tags = []
 
-        if entry_name.strip().replace("_", "").replace(".", "").isdigit():
-            file = file[:file.rfind("/")]
-            entry_name = file[file.rfind("/") + 1:]
-        if entry_ext.lower() in SUPPORTED_IMAGE_FORMATS:
-            entry_cover = file
-        elif entry_ext.lower() in SUPPORTED_VIDEO_FORMATS:
+        if entry_ext.lower() in SUPPORTED_VIDEO_FORMATS:
             entry_name, entry_author, entry_year, entry_genre = util.get_video_metadata(file)
             entry_cover = util.cache_video_cover(db.db_dir, CACHE_DIR, file)
             entry_res = util.get_video_resolution(file)
@@ -98,6 +93,7 @@ class Entry:
             entry_name, entry_author, entry_lang = util.get_epub_metadata(file)
             entry_tags = ['Book']
         elif entry_ext.lower() in SUPPORTED_IMAGE_FORMATS:
+            entry_cover = file
             entry_res = util.get_image_resolution(file)
             entry_tags = ['Image']
         elif entry_ext in SUPPORTED_AUDIO_FORMATS:
@@ -107,6 +103,10 @@ class Entry:
             entry_tags = ['Audio', entry_genre] if entry_genre is not None else ['Audio']
         elif entry_ext.lower() in SUPPORTED_TEXT_FORMATS:
             entry_tags = ['Text']
+
+        if file[file.rfind("/") + 1:file.rfind(".")].strip().replace("_", "").replace(".", "").isdigit():
+            file = file[:file.rfind("/")]
+            entry_name = file[file.rfind("/") + 1:]
 
         if entry_name is None:
             print("fix name")
@@ -334,75 +334,11 @@ class Database:
             if "._" in file:
                 print("found ._ in " + file)
                 continue
-
-            entry_name = file[file.rfind("/") + 1:file.rfind(".")]
-            entry_ext = file[file.rfind(".") + 1:]
-            entry_cover = "unknown"
-            entry_lang = None
-            entry_author = None
-            entry_series = None
-            entry_vol = 0
-            entry_year = 0
-            entry_res = (0, 0)
-            entry_tags = []
-            if entry_ext.lower() in SUPPORTED_IMAGE_FORMATS:
-                entry_cover = file
-            if entry_name.strip().replace("_", "").replace(".", "").isdigit():
-                file = file[:file.rfind("/")]
-                entry_name = file[file.rfind("/") + 1:]
             if file[len(self.db_dir):] in self.filepaths:
                 print("skip file -- known" + file)
                 continue
-            elif entry_ext.lower() in SUPPORTED_VIDEO_FORMATS:
-                entry_name, entry_author, entry_year, entry_genre = util.get_video_metadata(file)
-                entry_cover = util.cache_video_cover(self.db_dir, CACHE_DIR, file)
-                entry_res = util.get_video_resolution(file)
-                entry_tags = ['Video', entry_genre] if entry_genre is not None else ['Video']
-            elif entry_ext.lower() == "epub":
-                entry_cover = util.cache_epub_cover(self.db_dir, CACHE_DIR, file)
-                entry_name, entry_author, entry_lang = util.get_epub_metadata(file)
-                entry_tags = ['Book']
-            elif entry_ext.lower() in SUPPORTED_IMAGE_FORMATS:
-                entry_res = util.get_image_resolution(file)
-                entry_tags = ['Image']
-            elif entry_ext in SUPPORTED_AUDIO_FORMATS:
-                entry_name, entry_author, entry_series, entry_vol, entry_year, entry_genre = util.get_audio_metadata(
-                    file)
-                entry_cover = util.cache_audio_cover(self.db_dir, CACHE_DIR, file)
-                entry_tags = ['Audio', entry_genre] if entry_genre is not None else ['Audio']
-            elif entry_ext.lower() in SUPPORTED_TEXT_FORMATS:
-                entry_tags = ['Text']
 
-            if entry_name is None:
-                print("fix name")
-                entry_name = file[file.rfind("/") + 1:file.rfind(".")]
-            if entry_lang is None:
-                print("fix lang")
-                entry_lang = "unknown"
-            if entry_author is None:
-                print("fix author")
-                entry_author = "unknown"
-            if entry_series is None:
-                print("fix series")
-                entry_series = "unknown"
-            if entry_vol is None or not isinstance(entry_vol, int):
-                print("fix vol")
-                entry_vol = 0
-            if entry_year is None:
-                print("fix year")
-                entry_year = 0
-            elif not isinstance(entry_year, int):
-                print("fix string year")
-                entry_year = int(re.sub("[^0-9]", "", entry_year)[:4])
-            if len(entry_tags) <= 0:
-                print("fix tags")
-                entry_tags = ['Unknown']
-
-            print(entry_name, entry_lang, entry_author, entry_series, entry_vol, entry_lang, entry_year, entry_res,
-                  entry_tags)
-            entry = Entry(file[len(self.db_dir):], entry_cover, entry_name.replace("_", " "),
-                          entry_author, entry_series, int(entry_vol), entry_lang, "NA", int(entry_year),
-                          entry_res, entry_tags)
+            entry = Entry.from_file(self, file)
             print("created entry")
 
             self.add_entry(entry)
